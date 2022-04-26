b927092f3010aa3aea1990c7c2590099f807ad14
Shafran Naizer <mshafran13@gmail.com>
2022-04-27 00:36:09 +0400

    first commit

--- src/Components/common-button/CommonButton.js
+++ src/Components/common-button/CommonButton.js
@@ -2,12 +2,24 @@ import React from 'react'
 
 export default function CommonButton(props) {
 
-    let {onClick} =props
+    let { isLoading, onClick } = props
 
     return (
 
         <div>
-            <button onClick={onClick} className='h-12 w-full rounded-md bg-[#24E8A1] font-semibold hover:bg-green-400'>Send Message</button>
+            <button disabled={isLoading} onClick={onClick} className='h-12 w-full rounded-md bg-[#24E8A1] font-semibold hover:bg-green-400'>
+                {isLoading ?
+                    <span>
+                        Loading
+                    </span>
+
+                    :
+                    <span >
+                        Send Message
+                    </span>
+                }
+
+            </button>
         </div>
     )
 }
--- src/Layouts/FormLayout/FormLayout.js
+++ src/Layouts/FormLayout/FormLayout.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CommonButton from '../../Components/common-button/CommonButton';
 import CommonInput from '../../Components/common-input/CommonInput'
 import CommonTextArea from '../../Components/common-textarea/CommonTextArea'
@@ -6,11 +6,16 @@ import Spacer from '../../Components/spacer/Spacer';
 import { useForm, FormProvider, useFormContext } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
+import apiClient from "../../Services/http-common";
 
 
 export default function FormLayout() {
 
-    const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+    const [loading, setloading] = useState(false)
+    const [renderSucessAlert, setrenderSucessAlert] = useState(false)
+    const [renderErrorAlert, setrenderErrorAlert] = useState(false)
+
+    const phoneRegExp = /^((\\[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
 
     const schema = yup.object({
         fname: yup.string().required('First Name is a required field'),
@@ -18,18 +23,47 @@ export default function FormLayout() {
         email: yup.string().email().required('Email is not valid'),
         contact_no: yup.string().matches(phoneRegExp, 'Phone number is not valid'),
         desc: yup.string().required('Message is a required field')
-      });
+    });
 
-    const methods = useForm({ resolver: yupResolver(schema)});
+    const methods = useForm({ resolver: yupResolver(schema) });
     //const { conatctUsForm, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
+    const onSubmit = async (data) => {
+        console.log(data);
+        setloading(true)
+        try {
+            const res = await apiClient.post("/contact-us/submit-req", data, {
+                headers: {
+                    "x-access-token": "token-value",
+                },
+            });
+
+            console.log(res, 'responce');
+            setrenderSucessAlert(true)
+            setTimeout(() => {
+                setrenderSucessAlert(false)
+            }, 2000);
+
+            setloading(false)
+
+            methods.reset()
+        } catch (err) {
+            console.log(err);
+
+            setrenderErrorAlert(true)
+            setTimeout(() => {
+                setrenderErrorAlert(false)
+            }, 2000);
+
+            setloading(false)
+        }
+    }
 
 
     return (
         <>
             <FormProvider {...methods}>
                 <form onSubmit={methods.handleSubmit(onSubmit)}>
-                    <CommonInput id={'fname'}  lable={'First Name'} placeholder={'Jhon Carter'} isRequired={true} />
+                    <CommonInput id={'fname'} lable={'First Name'} placeholder={'Jhon Carter'} isRequired={true} />
 
                     <CommonInput id={'lname'} lable={'Last Name'} placeholder={'Silva'} isRequired={true} />
 
@@ -40,7 +74,26 @@ export default function FormLayout() {
                     <CommonTextArea id={'desc'} lable={'What is your concern?'} placeholder={'Tell Us!'} isRequired={true} />
 
                     <Spacer />
-                    <CommonButton onClick={() => onSubmit} />
+
+                    {renderErrorAlert ?
+                        <div className="px-4 py-3 leading-normal text-white bg-red-500 rounded-lg" role="alert">
+                            <p>Some thing Went Wrong!</p>
+                        </div>
+                        : null
+                    }
+
+                    {renderSucessAlert ?
+                        <div className="px-4 py-3 leading-normal text-white bg-green-500 rounded-lg" role="alert">
+                            <p>Request Sent Sucessfully!</p>
+                        </div>
+                        :
+                        null
+                    }
+
+
+                    <Spacer />
+
+                    <CommonButton isLoading={loading} onClick={() => onSubmit} />
                 </form>
 
             </FormProvider>
